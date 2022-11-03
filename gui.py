41e141d35c121875b63812ba365b45b39d7c917d
DJ Rice <deanrice44@gmail.com>
2022-11-03 09:25:43 -0400

    Renamed tkinter to gui because I have brain damage

--- gui.py
+++ gui.py
@@ -1,6 +1,6 @@
-import tkinter as tk
+from tkinter import *
 from tkinter import ttk
-root = tk()
+root = Tk()
 frm = ttk.Frame(root, padding=10)
 ttk.Label(frm, text="Burger king foot lettuce").grid(column=0, row=0)
 ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
