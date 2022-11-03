3d2380bdd4dbcd1eb706dba80267be7896c5928d
DJ Rice <deanrice44@gmail.com>
2022-11-03 09:27:27 -0400

    more gui testing fixes

--- gui.py
+++ gui.py
@@ -3,5 +3,6 @@ from tkinter import ttk
 root = Tk()
 frm = ttk.Frame(root, padding=10)
 ttk.Label(frm, text="Burger king foot lettuce").grid(column=0, row=0)
-ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
+ttk.Button(frm, text="Quit", command=root.destroy).grid(column=0, row=1)
+frm.pack()
 root.mainloop()
