349488ed34f931c166b168f27e27aea1179d6ac8
DJ Rice <deanrice44@gmail.com>
2022-11-08 22:59:01 -0500

    Got basic Google Calendar integration working

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from ics import Calendar
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
 from configparser import ConfigParser
 from datetime import datetime as dt
+from datetime import timedelta
 from datetime import date
 from datetime import time as t
+import pickle
 import time
 from os.path import exists
 from os import getcwd
@@ -78,27 +83,78 @@ class CalendarManager:
 
     #Constitutes the main loop for the program
     def run(self):
-        #TODO: Figure out a better way to loop this
-        while(True):
-            new = self.organize_events(ret_new=True)
-            print("Sending notification!")
-            notification.notify(
-                title = "Daily notification on {}".format(date.today()),
-                message = "Your three upcoming assignments are as follows:\n{} due {}\n{} due {}\n{} due {}".format(new[0].name, new[0].begin.humanize(),
-                                                                                                                    new[1].name, new[1].begin.humanize(),
-                                                                                                                    new[2].name, new[2].begin.humanize()),
-                timeout = 45
-            )
-            print("Sleeping...")
-            time.sleep(120)
+        #TODO: Implement a tkinter notification window
+        return
 
+    def get_calendar_service(self):
+        #This links up to the google calendar service
+        #Need to test what happens when multiple users are added
+        #Although god only knows if imma ever get there
+        SCOPES = ['https://www.googleapis.com/auth/calendar']
+        CREDENTIALS_FILE = 'bin/client_secret.json'
+
+        creds = None
+        #Checks to see if the token already exists
+        if exists("token.pickle"):
+            with open('token.pickle','rb') as token:
+                creds = pickle.load(token)
+
+        #If it doesn't, we make it or refresh it
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(CREDENTIALS_FILE, SCOPES)
+                creds = flow.run_local_server(port=0)
+        #i have come for your pickle
+            with open('token.pickle', 'wb') as token:
+                pickle.dump(creds, token)
+
+        service = build('calendar', 'v3', credentials=creds)
+        return service
+
+    #Does whatever I need it to
+    def debug(self):
+        service = self.get_calendar_service()
+        d = dt.now().date()
+        tomorrow = dt(d.year, d.month, d.day, 10)+timedelta(days=1)
+        start = tomorrow.isoformat()
+        end = (tomorrow+timedelta(hours=1)).isoformat()
+
+        event_result = service.events().insert(calendarId='primary',
+        body = {
+            "summary": "Test event",
+            "description": "Fortnite balls, im gay",
+            "start": {"dateTime": start, "timeZone": "America/New_York"},
+            "end": {"dateTime": end, "timeZone": "America/New_York"},
+        }
+        ).execute()
+
+        print("Created New Event: \n")
+        print("id: ", event_result['id'])
+        print("summary: ", event_result['summary'])
+        print("description: ", event_result['description'])
+        print('Start: ', event_result['start']['dateTime'])
+        print('End: ', event_result['end']['dateTime'])
+
+
+    #Old code saved for reference
+    #    service = self.get_calendar_service()
+    #    cal_res = service.calendarList().list().execute()
+    #
+    #    cals = cal_res.get('items', [])
+    #
+    #    if not cals:
+    #        print('No calendars found. The fuck?')
+    #    else:
+    #        for cal in cals:
+    #            summary = cal['summary']
+    #            id= cal['id']
+    #            primary = "Primary" if cal.get('primary') else ""
+    #            print("%s\t%s\t%s" % (summary, id, primary))
 
 if __name__ == "__main__":
 
     c = CalendarManager()
-    c.run()
-
-
-#for x in events:
-#    print("Event {} started {}".format(x.name, x.begin))
+    c.debug()
 
